f95947f3996d2eae570765ab917719779489db4c
joachim1202 <joachim.vermeire@student.vives.be>
2021-03-09 17:31:11 +0100

    eerste commit

--- TGV/Controllers/HomeController.cs
+++ TGV/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace TGV.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult PrivacyLave()
+        {
+            return View();
+        }
+
     }
 }
